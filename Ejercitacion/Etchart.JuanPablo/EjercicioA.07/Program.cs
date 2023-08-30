66c063c280dbe148e42fbbe51c12ddf896d3d768
Juan Pablo Etchart <juanetchart29@gmail.com>
2023-08-30 10:21:33 -0300

    corregido

--- Ejercitacion/Etchart.JuanPablo/EjercicioA.07/Program.cs
+++ Ejercitacion/Etchart.JuanPablo/EjercicioA.07/Program.cs
@@ -1,4 +1,13 @@
-﻿internal class Program
+﻿
+using System.Runtime.ConstrainedExecution;
+/// <summary>
+/// [A.07] ¿Cuántos días viviste?
+//Hacer un programa que pida por pantalla la fecha de nacimiento de una persona
+//(día, mes y año) y calcule el número de días vividos por esa persona hasta la fecha
+//actual (tomar la fecha del sistema con DateTime.Now).
+//Nota: Utilizar estructuras selectivas.Tener en cuenta los años bisiestos.
+/// </summary>
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -6,14 +15,12 @@
         Console.WriteLine("ingrese su fecha de nacimiento en dd/mm/year");
         EasyWay();
     }
+
     public static void EasyWay()
-    {
+    {    
         Console.WriteLine("ingrese su fecha de nacimiento en dd/mm/year");
         string fechaNacimiento =Console.ReadLine();
         Console.WriteLine($"{(DateTime.Now.Date - DateTime.Parse(fechaNacimiento)).Days}");
     }
-    public static void HardWay()
-    {
 
-    }
 }
